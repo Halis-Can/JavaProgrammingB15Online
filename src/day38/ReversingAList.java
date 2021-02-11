9c781367cde0cc442d3f08f062c479a84b245ab0
Halis-Can <haliscan@hotmail.com>
2021-02-10 19:57:02 -0500

    New Update

--- src/day38/ReversingAList.java
+++ src/day38/ReversingAList.java
@@ -12,16 +12,21 @@ public class ReversingAList {
         // reversing an ArrayList object
 
         List<Integer> nums1 = new ArrayList<>(Arrays.asList(200, 100, 700, 400, 500));
-        System.out.println("nums1 before reversing = " + nums1);
-        // 200, 100, 700, 400, 500
-        // after reversing
-        // 500 ,400 , 700, 100, 200
+        System.out.println("nums1 before reversing = " + nums1);//==>nums1 before reversing = [200, 100, 700, 400, 500]
+        Collections.sort(nums1);
         Collections.reverse(nums1);
-        System.out.println("nums1 after  reversing = " + nums1);
-        // after sorting in reverse order line 15
-        // 700, 500 , 400, 200 ,100
-        Collections.sort(nums1, Collections.reverseOrder()); // OR Comparator.reverseOrder()
-        System.out.println("nums1 after sorting reverse order = " + nums1);
+        System.out.println("nums1 after  reversing = " + nums1);//nums1 after  reversing = [500, 400, 700, 100, 200]
+        Collections.sort(nums1); // OR Comparator.reverseOrder()
+        System.out.println("nums1 after sorting  = " + nums1);
+                          //==>nums1 after sorting  = [100, 200, 400, 500, 700]
+        Collections.reverse(nums1);
+        System.out.println("nums1 after sorting reverse order = "+nums1);
+        //==>nums1 after sorting reverse order = [700, 500, 400, 200, 100]
+        //==Example======
+        List<Integer> nums2 = new ArrayList<>(Arrays.asList(222, 111, 777, 444, 555));
+        Collections.sort(nums2,Collections.reverseOrder());//==>one shot!!!!!!
+        System.out.println("num2 After sorting and  reverse order (one shot!!) "+nums2);
+        //==>num2 After sorting and  reverse order (one shot!!) [777, 555, 444, 222, 111]
 
     }
 
